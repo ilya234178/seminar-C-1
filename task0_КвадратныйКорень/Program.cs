b1c1b7db837d6dc8112b86592a67706d17fd42d6
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-07-10 22:42:10 +0400

    Квадрат числа

--- task0_КвадратныйКорень/Program.cs
+++ task0_КвадратныйКорень/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Clear();
+string str = "Привет дружок!";
+
+Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+// int numberA = 4;
+int result = numberA * numberA;
+Console.WriteLine($"Квадрат числа {numberA} = {result}");
